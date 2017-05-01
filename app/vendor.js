7dd17b1978f2932e96ad45eba430059de04aa212
Victor <Victor>
2017-05-01 13:14:39 +0300

    stable version

--- app/vendor.js
+++ app/vendor.js
@@ -1,12 +1,12 @@
 "use strict";
 
-import '../vendor/angular/angular.min.js';
-import '../vendor/angular-animate/angular-animate.min.js';
-import '../vendor/angular-mocks/angular-mocks.js';
-import '../vendor/angular-resource/angular-resource.min.js';
-import '../vendor/angular-route/angular-route.min.js';
-import '../vendor/angular-ui-router/release/angular-ui-router';
-import '../vendor/xml2json.min.js/xml2json.min.js';
-import './static/list/spell.min.js';
-import '../vendor/bootstrap/dist/js/bootstrap.min.js';
-import '../vendor/bootstrap/dist/css/bootstrap.min.css';
+import '../bower_components/angular/angular.min.js';
+import '../bower_components/angular-animate/angular-animate.min.js';
+import '../bower_components/angular-mocks/angular-mocks.js';
+import '../bower_components/angular-resource/angular-resource.min.js';
+import '../bower_components/angular-route/angular-route.min.js';
+import '../bower_components/angular-ui-router/release/angular-ui-router';
+import '../bower_components/xml2json.min.js/xml2json.min.js';
+import './core/spell-module/spell.bundle.js';
+import '../bower_components/bootstrap/dist/js/bootstrap.min.js';
+import '../bower_components/bootstrap/dist/css/bootstrap.min.css';
